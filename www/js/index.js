6b094605be32742fa3aa51b3c8ddb7a3fddd1dbe
Vlad-Ioan Talimba <talv1_18@uni.worc.ac.uk>
2019-10-17 14:19:53 +0100

    Initial commit

---
+++ www/js/index.js
@@ -0,0 +1,68 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var key1 = "phone";
+var value1 = "black";
+var key2 = "pen";
+var value2 = "blue";
+var key3 = "cable";
+var value3 = "white";
+var key4 = "wallet";
+var value4 = "black";
+var key5 = "watch";
+var value5 = "grey";
+
+window.localStorage.setItem(key1, value1);
+window.localStorage.setItem(key2, value2);
+window.localStorage.setItem(key3, value3);
+window.localStorage.setItem(key4, value4);
+window.localStorage.setItem(key5, value5);
+
+
+var value = window.localStorage.getItem (key1);
+alert (value);
+
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+app.initialize();
